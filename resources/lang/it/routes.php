bef404a76e23576168c4d4cde50a0b66903cbfa8
Davide Bronzini <68387610+ilBronza@users.noreply.github.com>
2023-12-05 22:25:28 +0100

    trans routes

---
+++ resources/lang/it/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return[
+
+	'header.hide' => 'Nascondi',
+	'header.hidePermanently' => 'Nasconi permanentemente',
+	'header.show' => 'Mosta',
+	'index' => 'Indice delle notifiche',
+	'store' => 'Crea notifica',
+	'archived' => 'Notifiche archiviate',
+	'create' => 'Crea notifica',
+	'list' => 'Lista notifiche',
+	'header.resetSessionCache' => 'Resetta la cache di sessione',
+	'show' => 'Mostra notifica',
+	'update' => 'Modifica notifica',
+	'destroy' => 'Elimina',
+	'archive' => 'Archivia',
+	'edit' => 'Modifica notifica',
+
+];
